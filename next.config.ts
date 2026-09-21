efa4d29f90f01927931d152ca714dceb90d87227
Sean Kim <sean.sk07@gmail.com>
2026-09-21 11:32:29 -0400

    commit new change

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
   },
   // Replace 'your-repo-name' with the exact name of your GitHub repository
-  basePath: '/next-portfolio', 
+  basePath: "/next-portfolio",
+};
+
+module.exports = {
+  allowedDevOrigins: ["10.90.70.144"],
 };
 
 export default nextConfig;
