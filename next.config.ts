e03629c34d0dda1ac33c0cee0bb4a21e8d67661b
Sean Kim <sean.sk07@gmail.com>
2026-09-21 11:33:59 -0400

    fixing github static pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  // Replace 'your-repo-name' with the exact name of your GitHub repository
   basePath: "/next-portfolio",
 };
 
