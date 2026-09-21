5e97573ada5788597b476d6d1a3a2d145631a823
Sean Siwon Kim <122237931+thexstriker@users.noreply.github.com>
2026-09-21 11:26:15 -0400

    Configure Next.js settings in next.config.ts
    Set output to 'export', enable unoptimized images, and define basePath.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Replace 'your-repo-name' with the exact name of your GitHub repository
+  basePath: '/next-portfolio', 
 };
 
 export default nextConfig;
